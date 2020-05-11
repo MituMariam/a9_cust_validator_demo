192784c1049b3a8cfd2c9eff48b3bdd2289c04af
unknown <mitumariam18@gmail.com>
2020-05-11 13:02:18 -0700

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { SignupComponent } from './auth/signup/signup.component';
+import { SigninComponent } from './auth/signin/signin.component';
+
+
+const routes: Routes = [
+  {
+    path:"signup", component: SignupComponent
+  },
+  {
+    path:"", component: SigninComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
